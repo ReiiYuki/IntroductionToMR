c1e7cb612408083741a22a3ec6949fba14c32dd2
Voraton Lertrattanapaisal <voraton.l@ku.th>
2017-06-07 21:27:23 -0700

    Play with rotate object

---
+++ Assets/MovingCube.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.left * Time.deltaTime * 0.5f);
+        transform.RotateAround(Camera.main.transform.position, Vector3.left, 5 * Time.deltaTime);
+        transform.RotateAround(Camera.main.transform.position, Vector3.down, 5 * Time.deltaTime);
+
+    }
+}
